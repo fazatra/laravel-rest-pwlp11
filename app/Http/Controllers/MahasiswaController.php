30e66aa739a6eadf7054c07a880a58e145f9e8ff
fazatra <farhanputra368@gmail.com>
2023-05-21 14:56:21 +0700

    RESTAPI

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+use App\Http\Resources\MahasiswaResource;
+use App\Http\Requests\StoreMahasiswaRequests;
+use App\Http\Requests\UpdateMahasiswaRequest;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return MahasiswaResource::collection(Mahasiswa::paginate());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMahasiswaRequests $request)
+    {
+        return new MahasiswaResource(Mahasiswa::create(
+        [
+        'Nim'=>$request->Nim,
+        'Nama'=>$request->Nama,
+        'Tanggal_Lahir'=>$request->Tanggal_Lahir,
+        'Jurusan'=>$request->Jurusan,
+        'No_Handphone'=>$request->gNo_Handphone,
+        'Email'=>$request->Email,
+        ]));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Mahasiswa  $mahasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mahasiswa $mahasiswa)
+    {
+        return new MahasiswaResource($mahasiswa);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Mahasiswa  $mahasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Mahasiswa  $mahasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMahasiswaRequest $request, Mahasiswa $mahasiswa)
+    {
+        $mahasiswa->update([
+        'Nim'=>$request->Nim,
+        'Nama'=>$request->Nama,
+        'Tanggal_Lahir'=>$request->Tanggal_Lahir,
+        'Jurusan'=>$request->Jurusan,
+        'No_Handphone'=>$request->gNo_Handphone,
+        'Email'=>$request->Email,
+        ]);
+        return new MahasiswaResource($mahasiswa);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Mahasiswa  $mahasiswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mahasiswa $mahasiswa)
+    {
+        $mahasiswa->delete();
+        return response()->noContent();
+    }
+}
